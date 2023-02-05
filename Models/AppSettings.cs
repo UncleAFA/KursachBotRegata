9dddc405694a8920bd5c40705e10ac62d50673f5
Алексей Федоров <vedopov2014@gmail.com>
2023-02-05 14:35:42 +0300

    Добавлено чтение и заполнение данных А еще скрипты для создания таблиц в бд И данные в файле в нужном формате

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -2,9 +2,9 @@
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://7c09-195-19-37-164.eu.ngrok.io" + "/{0}";
+        public static string Url { get; set; } = "https://ee75-90-154-71-80.eu.ngrok.io" + "/{0}";
         public static string Key { get; set; } = "5710419666:AAFwvJNvTu5qpJ4_1C2_6MZxHBbO4tZPRsE";
-        public static string ConnString { get; set; } = "Server=host.docker.internal;Username=postgres;Database=postgres;Port=49153;Password=postgrespw;SSLMode=Prefer";
+        public static string ConnString { get; set; } = "Server=host.docker.internal;Username=postgres;Database=postgres;Port=32769;Password=postgrespw;SSLMode=Prefer";
         
     }
 }
--- Models/Commands/StartCommand.cs
+++ Models/Commands/StartCommand.cs
@@ -1,8 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.Xml.Linq;
+using System.Linq;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using KursachBotRegata.Models;
 
 namespace KursachBotRegata.Models.Commands
 {
@@ -34,6 +40,7 @@ namespace KursachBotRegata.Models.Commands
                 );
                 Variables.StateList[message.Chat.Id] = Variables.State.GetLogin;
                 Variables.InputDataList[message.Chat.Id] = new InputInfo();
+                // readfile();
             }
             else{
                 var keyboard = new InlineKeyboardMarkup
@@ -50,6 +57,43 @@ namespace KursachBotRegata.Models.Commands
                     replyMarkup: keyboard
                 );
             }
+        }
+
+        public static void readfile()
+        {
+            string fileName = @"C:\2022.txt";
+
+            List<string> lines = System.IO.File.ReadLines(fileName).ToList();
+            // Console.WriteLine(String.Join(Environment.NewLine, lines));
+            foreach (var item1 in lines)
+            {
+                
+                string[] stringssss = item1.ToString().Split('|');
+                // if(stringssss.Length!= 4)
+                //     System.Console.WriteLine(item1);
+                string [] datatostr = stringssss[3].ToString().Split('/');
+                // System.Console.WriteLine(stringssss.Length);
+                // if(datatostr.Length!= 3)
+                //     System.Console.WriteLine(stringssss[3]);
+
+                // foreach (var item in datatostr)
+                // {
+                //     System.Console.WriteLine(item);
+                // }
+                DateTime test = new DateTime(int.Parse(datatostr[2].ToString()),int.Parse(datatostr[0].ToString()),int.Parse(datatostr[1].ToString()));
+                // System.Console.WriteLine(test);
+                DBWorker.InsertCommand("","listrecords",$@"'{stringssss[0].ToString().Trim(' ')}','3','{stringssss[1].ToString().Trim(' ')}','{stringssss[2].ToString().Trim(' ')}','{test.Year}-{test.Month}-{test.Day}'");
+
+                    
+            
+            }
+            System.Console.WriteLine("Всеееееееееееееееее");
+            // DBWorker.InsertCommand("","listrecords",$@"'{Variables.InputDataList[message.Message.Chat.Id].InsertFio}',+
+            //                                            '{Variables.InputDataList[message.Message.Chat.Id].Group}',
+            //                                            '{Variables.InputDataList[message.Message.Chat.Id].InsertPoint}',
+            //                                            '{Variables.InputDataList[message.Message.Chat.Id].InsertDetail}',
+            //                                            '{DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day}'");
+            
         }
     }
 }
