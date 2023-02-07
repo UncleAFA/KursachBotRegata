e7081318aa11607271a81bf3b1594a8a5ce1baad
Алексей Федоров <vedopov2014@gmail.com>
2023-02-07 22:57:22 +0300

    Test

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://ee75-90-154-71-80.eu.ngrok.io" + "/{0}";
+        public static string Url { get; set; } = "https://kursachbotregata20230207225331.azurewebsites.net/" + "/{0}";
         public static string Key { get; set; } = "5710419666:AAFwvJNvTu5qpJ4_1C2_6MZxHBbO4tZPRsE";
         public static string ConnString { get; set; } = "Server=host.docker.internal;Username=postgres;Database=postgres;Port=32769;Password=postgrespw;SSLMode=Prefer";
         
